e1e1cb3003e2b16002d0592f10cb6d02b2fc3481
renemonteiro <renemonteiro@hotmail.com.br>
2020-11-05 13:32:02 -0300

    migracao

---
+++ labenusystem/src/routes.ts
@@ -0,0 +1,2 @@
+import {json,Request, Response } from 'express'
+import knex from "./database/connection"
--- labenusystem/src/server.ts
+++ labenusystem/src/server.ts
@@ -1,25 +1,16 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
-import knex from 'knex'
-import dotenv from 'dotenv'
 
 
-dotenv.config()
-export const connection = knex ({
-    client:'mysql',
-    connection:{
-        host:process.env.DB_HOTS,
-        port:3306,
-        user: process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    }
-})
 
 
 const app: Express = express()
 app.use(express.json())
 app.use(cors())
+
+app.get("/", (request: Request, response: Response)=>{
+    return response.json({message: "oi"})
+})
 
 app.listen(3003, ()=>{
     console.log("Servidor Rodando")
